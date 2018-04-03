e10e484fc8d341b1287209314f870c64473fe0fc
coltonreeves <coltonallenreeves@gmail.com>
2018-04-03 16:22:22 -0400

    done

--- app.rb
+++ app.rb
@@ -3,14 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is Matthew"
+    "My name is Colton Reeves"
   end
 
   get '/hometown' do
-    "My hometown is Port St. Lucie"
+    "My hometown is Cumming, GA"
   end
 
   get '/favorite-song' do
-    "My favorite song is adfadsf"
-  end
+    "My favorite song is 'Nights' by Frank Ocean"
+  end 
 end
